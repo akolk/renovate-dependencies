8a25a73ddaec5c0d5ce19915d41da59fbc43bf3e
Anjo Kolk <anjo.kolk@gmail.com>
2024-07-26 23:26:43 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    "endpoint": "https://git.dev.cloudi.kadaster.nl/api/v1", // replace it with your actual endpoint
+    "gitAuthor": "Renovate Bot <renovate-bot@yourhost.com>",
+    "platform": "gitea",
+    "onboardingConfigFileName": "renovate.json",
+    "autodiscover": true,
+    "optimizeForDisabled": true,
+};
