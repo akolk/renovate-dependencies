539d379ea323b3303948ddae9d4a171e01324a2c
Anjo Kolk <anjo.kolk@gmail.com>
2024-07-27 16:09:49 +0200

    Create parserenovate.py

---
+++ parserenovate.py
@@ -0,0 +1,30 @@
+import json
+
+with open(file='logfile', mode='r') as f:
+    lines = f.readlines()
+    f.close()
+
+for line in lines:
+    obj = json.loads(line)
+    if obj["msg"] == "packageFiles with updates":
+        # this object holds dependency information
+        project = obj['repository']
+        print("project: "+project)
+        managerlst = obj['config']
+        for manager in managerlst:
+            # interate over dependencies managed with this manager
+            print("    - manager: "+manager)
+            deplst = obj['config'][manager][0]['deps']
+            for dep in deplst:
+                # print(dep)
+                # print dependency
+                try:
+                    if len(dep['updates']) > 0:
+                       print("        - "+dep['depName']+" "+dep['currentVersion']+" "+ dep['updates'][0]['bucket'] + " " + dep['updates'][0]['newVersion'])
+                    else:
+                       print("        - "+dep['depName']+" "+dep['currentVersion'])
+                except:
+                    try:
+                        print("        - "+dep['replaceString'])
+                    except:
+                        print("        - <unknown>")
